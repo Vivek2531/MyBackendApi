26b1ce9d3190e5a2e053955c535c0c4b67db0c7c
Vivek2531 <vivekinturi27@gmail.com>
2025-04-19 22:01:43 -0400

    Add project files.

---
+++ LambdaEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MyBackendApi
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
